297e40f70a3359c9a9b7aacbb21f5a072555d553
Jarle Nygård <Jarle.Nygard@powel.no>
2018-06-05 15:03:49 +0200

    Initial commit

---
+++ src/P2P_Demo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P2P_Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
