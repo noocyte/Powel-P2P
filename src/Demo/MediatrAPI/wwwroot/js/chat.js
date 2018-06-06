ed20ae8df19e493b1887a58f786c7be9765bc5af
Jarle Nygård <Jarle.Nygard@powel.no>
2018-06-06 12:36:43 +0200

    Clean views

--- src/Demo/MediatrAPI/wwwroot/js/chat.js
+++ src/Demo/MediatrAPI/wwwroot/js/chat.js
@@ -1,4 +1,4 @@
-﻿// The following sample code uses modern ECMAScript 6 features 
+// The following sample code uses modern ECMAScript 6 features 
 // that aren't supported in Internet Explorer 11.
 // To convert the sample for environments that do not support ECMAScript 6, 
 // such as Internet Explorer 11, use a transpiler such as 
@@ -20,7 +20,9 @@ connection.on("ReceiveValue", (message) => {
 connection.start().catch(err => console.error(err.toString()));
 
 document.getElementById("sendButton").addEventListener("click", event => {
-    const message = document.getElementById("messageInput").value;
+    const inputBox = document.getElementById("messageInput");
+    const message = inputBox.value;
+    inputBox.value = "";
     connection.invoke("SendValue", message).catch(err => console.error(err.toString()));
     event.preventDefault();
 });
